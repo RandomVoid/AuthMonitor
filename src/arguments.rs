a0fcdbed05ad34404fb06e6a36bdc3e5996051e9
FastAlien <3268092+FastAlien@users.noreply.github.com>
2024-02-10 13:31:19 +0100

    Refactor arguments::parse_arguments and add unit tests

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,6 +1,16 @@
 use std::error::Error;
+use std::str::FromStr;
 
 use crate::auth_monitor::AuthMonitorParams;
+
+const OPTION_PREFIX: &str = "--";
+const OPTION_PREFIX_LENGTH: usize = OPTION_PREFIX.len();
+
+const OPTION_VALUE_SEPARATOR: char = '=';
+const OPTION_VALUE_SEPARATOR_LENGTH: usize = 1;
+
+const MAX_FAILED_ATTEMPTS_OPTION: &str = "max-failed-attempts";
+const RESET_AFTER_SECONDS_OPTION: &str = "reset-after-seconds";
 
 const DEFAULT_MAX_FAILED_ATTEMPTS: i32 = 3;
 const DEFAULT_RESET_AFTER_SECONDS: i32 = 30 * 60;
@@ -9,46 +19,65 @@ pub fn parse_arguments(arguments: &[String]) -> Result<AuthMonitorParams, Box<dy
     let mut filepath: Option<String> = None;
     let mut max_failed_attempts = DEFAULT_MAX_FAILED_ATTEMPTS;
     let mut reset_after_seconds = DEFAULT_RESET_AFTER_SECONDS;
-    let mut iterator = arguments.iter();
-    while let Some(arg) = iterator.by_ref().next() {
-        if !arg.starts_with("--") {
+    let arguments_iterator = arguments.iter();
+
+    for argument in arguments_iterator {
+        if !argument.starts_with(OPTION_PREFIX) {
             if filepath.is_some() {
                 return Err("File path specified more than once")?;
             }
-            filepath = Some(String::from(arg));
+            filepath = Some(String::from(argument));
             continue;
         }
-        let mut arguments = arg.split('=');
-        let name = match arguments.next() {
-            Some(name) => String::from_iter(name.chars().skip(2)),
-            None => Err("Missing option name")?,
-        };
-        let value = match arguments.next() {
-            Some(value) => value,
-            None => Err(format!("Missing value for option {}", name))?,
-        };
-        match name.as_str() {
-            "max-failed-attempts" => {
-                max_failed_attempts = match value.parse::<i32>() {
-                    Ok(value) => value,
-                    Err(error) => Err(format!("Invalid value for option {}: {}", name, error))?,
-                };
+        let (option_name, option_value) = match argument.find(OPTION_VALUE_SEPARATOR) {
+            Some(separator_position) => {
+                let (name, value) = argument.split_at(separator_position);
+                (name, Some(&value[OPTION_VALUE_SEPARATOR_LENGTH..]))
             }
-            "reset-after-seconds" => {
-                reset_after_seconds = match value.parse::<i32>() {
-                    Ok(value) => value,
-                    Err(error) => Err(format!("Invalid value for option {}: {}", name, error))?,
-                };
+            None => (argument.as_str(), None),
+        };
+        match &option_name[OPTION_PREFIX_LENGTH..] {
+            MAX_FAILED_ATTEMPTS_OPTION => {
+                max_failed_attempts = parse_option_value(option_name, option_value)?;
             }
-            _ => Err(format!("Unknown option \"{}\"", name))?,
+            RESET_AFTER_SECONDS_OPTION => {
+                reset_after_seconds = parse_option_value(option_name, option_value)?;
+            }
+            _ => Err(format!("Unknown option {}", argument))?,
         }
     }
+
     if filepath.is_none() {
         return Err("File path not specified")?;
     }
+
     return Ok(AuthMonitorParams {
         filepath: filepath.unwrap(),
         max_failed_attempts,
         reset_after_seconds,
     });
 }
+
+fn parse_option_value<T: FromStr>(
+    name: &str,
+    optional_value: Option<&str>,
+) -> Result<T, Box<dyn Error>> {
+    let value = match optional_value {
+        Some(value) => value,
+        None => Err(format!("Missing value for option {}", name))?,
+    };
+    if value.is_empty() {
+        return Err(format!("Missing value for option {}", name))?;
+    }
+    return match value.parse::<T>() {
+        Ok(value) => Ok(value),
+        Err(_) => Err(format!(
+            "\"{}\" is not a valid value for option {}",
+            value, name
+        ))?,
+    };
+}
+
+#[cfg(test)]
+#[path = "./arguments_test.rs"]
+mod test;
---
+++ src/arguments_test.rs
@@ -0,0 +1,125 @@
+#[cfg(test)]
+use crate::arguments::{
+    parse_arguments, DEFAULT_MAX_FAILED_ATTEMPTS, DEFAULT_RESET_AFTER_SECONDS,
+    MAX_FAILED_ATTEMPTS_OPTION, RESET_AFTER_SECONDS_OPTION,
+};
+
+const FILEPATH: &str = "/var/log/auth.log";
+
+#[test]
+fn parse_no_arguments() {
+    let arguments = [];
+    match parse_arguments(&arguments) {
+        Ok(_) => panic!("Error was expected"),
+        Err(error) => assert_eq!(error.to_string(), "File path not specified"),
+    }
+}
+
+#[test]
+fn parse_filepath_without_options() {
+    let arguments = [String::from(FILEPATH)];
+    test_parse_arguments(
+        &arguments,
+        FILEPATH,
+        DEFAULT_MAX_FAILED_ATTEMPTS,
+        DEFAULT_RESET_AFTER_SECONDS,
+    );
+}
+
+fn test_parse_arguments(
+    arguments: &[String],
+    filepath: &str,
+    max_failed_attempts: i32,
+    reset_after_seconds: i32,
+) {
+    match parse_arguments(arguments) {
+        Ok(params) => {
+            assert_eq!(params.filepath, filepath);
+            assert_eq!(params.max_failed_attempts, max_failed_attempts);
+            assert_eq!(params.reset_after_seconds, reset_after_seconds);
+        }
+        Err(error) => panic!("Error: {}", error),
+    }
+}
+
+#[test]
+fn parse_options_with_correct_values() {
+    let options = [MAX_FAILED_ATTEMPTS_OPTION, RESET_AFTER_SECONDS_OPTION];
+    for option in options {
+        let value = 30;
+        let option_argument = format!("--{}={}", option, value);
+        let arguments = [String::from(FILEPATH), option_argument];
+        let max_failed_attempts = match option == MAX_FAILED_ATTEMPTS_OPTION {
+            true => value,
+            false => DEFAULT_MAX_FAILED_ATTEMPTS,
+        };
+        let reset_after_seconds = match option == RESET_AFTER_SECONDS_OPTION {
+            true => value,
+            false => DEFAULT_RESET_AFTER_SECONDS,
+        };
+        test_parse_arguments(
+            &arguments,
+            FILEPATH,
+            max_failed_attempts,
+            reset_after_seconds,
+        );
+    }
+}
+
+#[test]
+fn parse_option_without_value() {
+    let options = [MAX_FAILED_ATTEMPTS_OPTION, RESET_AFTER_SECONDS_OPTION];
+    for option in options {
+        let option_arguments = [format!("--{}", option), format!("--{}=", option)];
+        for option_argument in option_arguments {
+            let arguments = [String::from(FILEPATH), option_argument];
+            match parse_arguments(&arguments) {
+                Ok(_) => panic!("Error was expected"),
+                Err(error) => {
+                    assert_eq!(
+                        error.to_string(),
+                        format!("Missing value for option --{}", option)
+                    );
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn parse_option_with_invalid_value() {
+    let options = [MAX_FAILED_ATTEMPTS_OPTION, RESET_AFTER_SECONDS_OPTION];
+    for option in options {
+        let values = ["test", "3a", "--", "b23", "12@"];
+        for value in values {
+            let option_argument = format!("--{}={}", option, value);
+            let arguments = [String::from(FILEPATH), option_argument];
+            match parse_arguments(&arguments) {
+                Ok(_) => panic!("Should return an error"),
+                Err(error) => {
+                    assert_eq!(
+                        error.to_string(),
+                        format!("\"{}\" is not a valid value for option --{}", value, option)
+                    );
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn parse_multiple_options() {
+    let max_failed_attempts = 10;
+    let reset_after_seconds = 3600;
+    let arguments = [
+        String::from(FILEPATH),
+        format!("--{}={}", MAX_FAILED_ATTEMPTS_OPTION, max_failed_attempts),
+        format!("--{}={}", RESET_AFTER_SECONDS_OPTION, reset_after_seconds),
+    ];
+    test_parse_arguments(
+        &arguments,
+        FILEPATH,
+        max_failed_attempts,
+        reset_after_seconds,
+    );
+}
