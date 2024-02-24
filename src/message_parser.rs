3e87a64baad6fbc41fa4e6119853ab54fc9c88d1
FastAlien <3268092+FastAlien@users.noreply.github.com>
2024-02-24 13:21:34 +0100

    Refactor message_parser::is_auth_failed_message()

--- src/message_parser.rs
+++ src/message_parser.rs
@@ -3,17 +3,12 @@ const PAM_PREFIX_LENGTH: usize = PAM_PREFIX.len();
 const AUTH_FAILURE_MESSAGE: &str = "authentication failure";
 
 pub fn is_auth_failed_message(message: &str) -> bool {
-    let prefix_position = find_pam_prefix_end(message);
-    if prefix_position.is_none() {
-        return false;
-    }
-    let (_, after_prefix_part) = message.split_at(prefix_position.unwrap());
-    return after_prefix_part.contains(AUTH_FAILURE_MESSAGE);
-}
-
-fn find_pam_prefix_end(message: &str) -> Option<usize> {
-    let position = message.find(PAM_PREFIX)?;
-    return Some(position + PAM_PREFIX_LENGTH);
+    return match message.find(PAM_PREFIX) {
+        None => false,
+        Some(pam_prefix_position) => {
+            message[pam_prefix_position + PAM_PREFIX_LENGTH..].contains(AUTH_FAILURE_MESSAGE)
+        }
+    };
 }
 
 #[cfg(test)]
